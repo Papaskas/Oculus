7750c9227e553209d49943fc1ca2724f652f2870
papaskas <pavel.glugxix@gmail.com>
2024-02-16 13:56:05 +0300

    Delete vernamCipher, added docker compose

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import 'react-toastify/dist/ReactToastify.css';
-import 'react-toastify/dist/ReactToastify.min.css';
-import Notification from '@/lib/Notification';
 
 
 function Index() {
   return (
-    <>
-      <button onClick={() => Notification.warning('text')}>Notify !</button>
-    </>
+    <div>home page</div>
   );
 }
 
